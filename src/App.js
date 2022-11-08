e3115ab62c6f162dcda1b1bf99012d9cfab1bc5a
victorfs32 <111938821+victorfs32@users.noreply.github.com>
2022-11-08 00:23:27 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   // variaveis iniciais 
   const [id, setId] = useState("1");
   const [img, setImg] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg");
-  const [nome, setNome] = useState("buba");
+  const [nome, setNome] = useState("bulbasaur");
   const [Hp, setHp] = useState("45");
   const [Atk, setAtk] = useState("49");
   const [Def, setDef] = useState("49");
@@ -37,54 +37,53 @@ function App() {
 
   return (
   
-    <div className="content">
-        <div className="card">
-        <form onSubmit={pesquisar}>
-              <input type="text" className="form" placeholder="ID ou Nome" aria-label="ID ou Nome" aria-describedby="button-addon2" />
-              <button className="btn btn-primary" type="submit" id="button-addon2">Buscar</button> 
-            </form>
-            <div className="topCard">
-                <h2 className="title">#{id}</h2>
-                <h3 className="title">{nome}</h3>
-                <span className="secondText"></span>
-            </div>
-            <div className="mediaCard">
+            <div className="content">
+              <div className="card">
+                <form className='form' onSubmit={pesquisar}>
+                    <input type="text" className="lo" placeholder="ID ou Nome" aria-label="ID ou Nome" aria-describedby="ID ou Nome" />
+                      <button className="la" type="submit" id="submit">Buscar</button> 
+                        </form>
+                          <div className="topCard">
+                          <h2 className="title">#{id}</h2>
+                        <h3 className="title">{nome}</h3>
+                    </div>
+                  <div className="mediaCard">
                 <img src={img} alt="logo"/>
                 </div>
                 <div className='progress-title'>
                   <h5>Hp</h5>
-                  <div className='progress'>
+                    <div className='progress'>
                       <div className='progress-bar hp' style={{width: `${Hp}%`}}>
-                          <div className='progress-value'>{Hp}%</div>
+                          <div className='progress-value'>{Hp}</div>
                       </div>
                   </div>
-                </div>
-                <div className='progress-title'>
-                  <h5>Attack</h5>
-                  <div className='progress'>
-                      <div className='progress-bar atk' style={{width: `${Atk}%`}}>
-                          <div className='progress-value'>{Atk}%</div>
-                      </div>
                   </div>
-                </div>
-                <div className='progress-title'>
+                  <div className='progress-title'>
+                    <h5>Attack</h5>
+                      <div className='progress'>
+                        <div className='progress-bar atk' style={{width: `${Atk}%`}}>
+                          <div className='progress-value'>{Atk}</div>
+                        </div>
+                    </div>
+                  </div>
+                  <div className='progress-title'>
                   <h5>Defense</h5>
-                  <div className='progress'>
+                    <div className='progress'>
                       <div className='progress-bar def' style={{width: `${Def}%`}}>
-                          <div className='progress-value'>{Def}%</div>
+                        <div className='progress-value'>{Def}</div>
                       </div>
+                    </div>
                   </div>
-                </div>
-                <div className='progress-title'>
+                  <div className='progress-title'>
                   <h5>Speed</h5>
-                  <div className='progress'>
+                    <div className='progress'>
                       <div className='progress-bar vel' style={{width: `${Vel}%`}}>
-                          <div className='progress-value'>{Vel}%</div>
+                        <div className='progress-value'>{Vel}</div>
                       </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
   );
 }
 
